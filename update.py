412059da0b493adf14048306d7bd8091de846276
Teruteru21623 <116567168+Teruteru21623@users.noreply.github.com>
2023-02-20 22:16:33 +0900

    ガチャのデータベース更新プログラム作成 岡山

---
+++ update.py
@@ -0,0 +1,29 @@
+#ガチャの中身を更新するためのプログラム
+
+from cs50 import SQL
+
+def main():
+    open("gacha.db", "w").close()
+    db = SQL("sqlite:///gacha.db")
+
+    #データベースを作成
+    db.execute("CREATE TABLE gachas(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, choice TEXT)")
+
+    #choice.txtを開く
+    with open("choice.txt", "r") as file:
+
+        #fileを行ごとに分割しリスト化する
+        datalist = file.readlines()
+
+        for data in datalist:
+            #dataがすでにデータベースに存在しないかを調べる
+            rows = db.execute("SELECT * FROM gachas WHERE choice = ?", data)
+            if len(rows) == 1:
+                continue
+
+            #存在しなければデータベースに追加
+            else:
+                db.execute("INSERT INTO gachas (choice) VALUES(?)", data)
+
+
+main()

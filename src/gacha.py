988c2e13125120b0df5fbbc3975c4582991fafaf
AA076-Riko <118287623+AA076-Riko@users.noreply.github.com>
2023-02-25 13:57:24 +0900

    修正

--- src/gacha.py
+++ src/gacha.py
@@ -1,18 +1,26 @@
 import sqlite3
 import random
 
+get_all_item()
+
 def get_all_item():
     conn = sqlite3.connect("gacha.db")
+    conn.row_factory = sqlite3.Row
     cur = conn.cursur()
     cur.execute("select * from gachas")
     result = [dict(row) for row in cur.fetchall()]
 
+    print(result)
     conn.close()
     return result
+
 
 def random_choice(result):
     todays_choice = random.sample(result,k=3)
     choiceA = todays_choice["0"]
     choiceB = todays_choice["1"]
     choiceC = todays_choice["2"]
+
+    return choiceA,choiceB,choiceC
+
 

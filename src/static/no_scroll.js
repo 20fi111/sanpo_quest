95d200a25a74644eb274a247478fff3fd4c49439
20fi111 <117149286+20fi111@users.noreply.github.com>
2023-03-16 01:02:42 +0900

    デザイン変更

--- src/static/no_scroll.js
+++ src/static/no_scroll.js
@@ -1,21 +1,9 @@
-if(document.URL.match(/quest)){
-    // スクロール開放
-    body.style.top = '';
-    body.classList.remove('no_scroll');
-    window.scrollTo(0, scrollTop);
-  } else {
-    // スクロール禁止
-    scrollTop = window.scrollY;
-    body.style.top = (scrollTop * -1) + 'px';
-    body.classList.add('no_scroll');
-  }
-});
 
 window.onload = ()=> {
     // パスの取得
+    const body = document.getElementsByTagName('body')[0];
     let path = location.pathname
-    if (path == "/quest.html") {
-      // ドメイン以下のパス名が /hoge/hoge.html の場合に実行する処理
+    if (path == "/quest") {
       // スクロール開放
     body.style.top = '';
     body.classList.remove('no_scroll');
